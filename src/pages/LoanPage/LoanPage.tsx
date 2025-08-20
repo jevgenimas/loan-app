23a83eebc241a3ed7e0aa952f22bea4a05dbe7ee
Jevgeni <jevgeni.massalov@gmail.com>
2025-08-20 14:45:03 +0300

    The code has been corrected

--- src/pages/LoanPage/LoanPage.tsx
+++ src/pages/LoanPage/LoanPage.tsx
@@ -5,27 +5,27 @@ import ImageElement from "../../components/ImageElement/ImageElement";
 import {scrollToElement} from "../../util/elementUtils";
 
 interface LoanCalculatorProps {
-  showLoginPopup: (amount: number, period: number, monthlyPayment: string) => void;
+    showLoginPopup: (amount: number, period: number, monthlyPayment: string) => void;
 }
 
 const LoanPage: React.FC<LoanCalculatorProps> = ({showLoginPopup}) => {
 
-  const loanCalculatorRef = useRef<HTMLDivElement>(null);
+    const loanCalculatorRef = useRef<HTMLDivElement>(null);
 
-  return (
-      <div className="LoanPage">
-        <LoanCalculator ref={loanCalculatorRef}
-                        showLoginPopup={showLoginPopup}/>
-        <ImageElement id={'image-1'}
-                      buttonText={'Apply for loan'}
-                      imageRef={'photo-1.jpg'}
-                      onClickButton={scrollToElement(loanCalculatorRef)}/>
-        <ImageElement id={'image-2'}
-                      buttonText={'Apply for loan'}
-                      imageRef={'photo-2.jpg'}
-                      onClickButton={scrollToElement(loanCalculatorRef)}/>
-      </div>
-  );
+    return (
+        <div className="LoanPage">
+            <LoanCalculator ref={loanCalculatorRef}
+                            showLoginPopup={showLoginPopup}/>
+            <ImageElement id={'image-1'}
+                          buttonText={'Apply for loan'}
+                          imageRef={'photo-1.jpg'}
+                          onClickButton={scrollToElement(loanCalculatorRef)}/>
+            <ImageElement id={'image-2'}
+                          buttonText={'Apply for loan'}
+                          imageRef={'photo-2.jpg'}
+                          onClickButton={scrollToElement(loanCalculatorRef)}/>
+        </div>
+    );
 };
 
 export default LoanPage;
